d1ba327d4c2902f5dacf13611e2cf751ad9658c2
chiqian0412 <102497006+chiqian0412@users.noreply.github.com>
2022-05-03 16:06:55 -0400

    upload memory game

--- exercise/MemoryCard/new.js
+++ exercise/MemoryCard/new.js
@@ -1,44 +1,43 @@
 const gameBoard = document.querySelector('.gameBoard');
-const score = document.querySelector('.score span');
-
-var timer= document.querySelector('.timer');       
-var maxtime = 20;  
-function CountDown() {
-    if (maxtime >= 0) {
-        msg = "Countdown:" + maxtime + "s";
-        timer.innerHTML = msg;
-        if(maxtime === 0 ){
-            alert("Time Over!");
-        } 
-    }
-}    
-var timers = setInterval("CountDown()", 1000); 
+const score = document.querySelectorAll('.scores');//array
+console.log(score[0].innerHTML)
+var playerOneScore = 0;
+var playerTwoScore = 0;
+var totle = 0;
+gameBoard.innerHTML = 
+`<div class="rules">
+<h3> Memory Game Rules:</h3>
+<p>Click two cards to match the same pictures.</p>
+<p>Match one pair to get 10 points;</p>
+<p>Every remaining second get 1 extra point.</p>
+</div>`
 
 // Classes the cards and back card.
-class card{
+class Card{
     constructor(name,src){
         this.name = name
         this.src = src
+        this.wasMatch = false
     }
 }
-const card1 = new card('card1', './Image/1.jpeg')
-const card2 = new card('card2', './Image/2.jpeg')
-const card3 = new card('card3', './Image/3.jpeg')
-const card4 = new card('card4', './Image/4.jpeg')
-const card5 = new card('card5', './Image/5.jpeg')
-const card6 = new card('card6', './Image/6.jpeg')
-const card7 = new card('card7', './Image/7.jpeg')
-const card8 = new card('card8', './Image/8.jpeg')
-const card9 = new card('card9', './Image/1.jpeg')
-const card10 = new card('card10', './Image/2.jpeg')
-const card11 = new card('card11', './Image/3.jpeg')
-const card12 = new card('card12', './Image/4.jpeg')
-const card13 = new card('card13', './Image/5.jpeg')
-const card14 = new card('card14', './Image/6.jpeg')
-const card15 = new card('card15', './Image/7.jpeg')
-const card16 = new card('card16', './Image/8.jpeg')
+const card1 = new Card('card1', './Image/1.jpeg')
+const card2 = new Card('card2', './Image/2.jpeg')
+const card3 = new Card('card3', './Image/3.jpeg')
+const card4 = new Card('card4', './Image/4.jpeg')
+const card5 = new Card('card5', './Image/5.jpeg')
+const card6 = new Card('card6', './Image/6.jpeg')
+const card7 = new Card('card7', './Image/7.jpeg')
+const card8 = new Card('card8', './Image/8.jpeg')
+const card9 = new Card('card9', './Image/1.jpeg')
+const card10 = new Card('card10', './Image/2.jpeg')
+const card11 = new Card('card11', './Image/3.jpeg')
+const card12 = new Card('card12', './Image/4.jpeg')
+const card13 = new Card('card13', './Image/5.jpeg')
+const card14 = new Card('card14', './Image/6.jpeg')
+const card15 = new Card('card15', './Image/7.jpeg')
+const card16 = new Card('card16', './Image/8.jpeg')
 const cards = [card1,card2,card3,card4,card5,card6,card7,card8,card9,card10,card11,card12,card13,card14,card15,card16]
-const backCard = new card('backCard','./Image/questionMark.jpeg')
+const backCard = new Card('backCard','./Image/questionMark.jpeg')
 
 
 // create a shuffle card function.
@@ -47,28 +46,116 @@ const shuffle = () =>{
     console.log(cards)
     return cards;
 }
+// create the timing function.
+var timer= document.querySelector('.timer');    
+timer.innerHTML = "Limite time:20s"
+var maxtime = 1000;  
+function timing(){
+    countDown = setInterval(() =>{
+        maxtime -= 1;
+        if (maxtime >= 0) {
+            msg = "Limite time:" + maxtime + "s";
+            timer.innerHTML = msg;
+            if(totle===80){
+                clearInterval(countDown);
+                totle = totle+ maxtime;
+                // score[player-1].innerHTML= `Player${player} Scores:+ ${totle}`
+            } else if(maxtime === 0 ) {
+                alert("Time Over!");
+            } 
+        }
+    }, 1000); 
+} 
+
+//create the flip card function.
+
+
 
 
 // create a generate function.
-const generateCards = (a) => {
-    // gameBoard.innerHTML = '';
+const generateCards = () => {
     shuffle()
+    gameBoard.innerHTML = "";
 
-    cards.forEach((a) => {
+    cards.forEach((card,index) => {
         const cardItem = document.createElement('div');
         cardItem.classList = 'cardItem';
         const frontView = document.createElement('img');
         frontView.classList = 'front';
+        frontView.addEventListener("click",() => {flipCard(card,index)});
         // const backView = document.createElement('img');
         // backView.classList = 'back';
         gameBoard.appendChild(cardItem);
         cardItem.appendChild(frontView);
         // cardItem.appendChild(backView);
-        frontView.src = a.src;
+        // frontView.src = index.src;
+        frontView.src = backCard.src;
         // backView.src = backCard.src;
     })
 
-timers;
+    var frontViews = document.querySelectorAll(".front");
+    var curCards = []; // two flip cards' index array.
+    function flipCard(card,index){
+        console.log(index);
+        frontViews[index].src = cards[index].src; // click the card to show the animals.
+        curCards.push(index);
+        if(curCards.length === 2){
+            console.log('bbb',curCards[0])
+            console.log('aaa',frontViews[0])
+            if(cards[curCards[0]].wasMatch ===false &&cards[curCards[1]].wasMatch ===false && (frontViews[curCards[0]] !== frontViews[curCards[1]])){
+
+                // two cards not match then cover again.
+                if(frontViews[curCards[0]].src !== frontViews[curCards[1]].src ){
+                   
+                        console.log(frontViews[0], backCard.src,curCards[1])
+                        frontViews[curCards[0]].src = backCard.src;
+                        setTimeout(()=>{
+                            this.frontViews[curCards[1]].src = backCard.src;
+                        },1000);
+                   
+                }else {
+                    // match than add scores.
+                    totle += 10;
+                    console.log('score',totle)
+                    cards[curCards[0]].wasMatch = true;
+                    cards[curCards[1]].wasMatch = true;
+                // score[0].innerHTML = 'Player1 Scores:'+ playerOneScore;
+                } 
+            }
+            else{
+                flipCard(card,index);
+            }
+            
+            curCards.splice(0,2);
+        }
+    }
+    //  when player click the start, the timer will count down.
+    countDown = setInterval(() =>{
+        maxtime -= 1;
+        if (maxtime >= 0) {
+            msg = "Limite time:" + maxtime + "s";
+            timer.innerHTML = msg;
+            if(totle===80){
+                clearInterval(countDown);
+                totle = totle+ maxtime;
+                console.log('intiming',totle)
+                // score[player-1].innerHTML= `Player${player} Scores:+ ${totle}`
+            } else if(maxtime === 0 ) {
+                alert("Time Over!");
+            } 
+        }
+    }, 1000); 
 }
-// generateCards();
-// console.log(gameBoard.innerHTML);
+
+const playerOneStart = () => {
+    generateCards()
+    
+    console.log('last',totle)
+    console.log(score[0])
+    // score[0].innerHTML = 'Player1 Scores:'+ totle;
+}
+
+const playerTwoStart = () => {
+    generateCards()
+    score[1].innerHTML = 'Player2 Scores:'+ totle;
+}

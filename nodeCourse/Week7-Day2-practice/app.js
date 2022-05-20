71263bf14b018851b305ae5346b8a4d1f318f7eb
chiqian0412 <qianchi0412@gmail.com>
2022-05-19 21:38:52 -0400

    complete the learner API

--- nodeCourse/Week7-Day2-practice/app.js
+++ nodeCourse/Week7-Day2-practice/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 // const learnerControllers = require('./controllers/learnerControllers.js')
 
-
 const learnerRouter = require('./routes/learnerRoutes')
 
-
+// create a middleware toparse the request body.
+app.use(express.json());
 // Create a middleWare to handle route /api/v1/learners/
 // /api/v1/learners/:id
 app.use("/api/v1/learners",learnerRouter);
